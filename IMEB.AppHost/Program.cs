e1b59cae97204f0e4c5cfced98d206923655f866
theana <41345545+theanaverwaerde@users.noreply.github.com>
2024-07-31 20:34:02 +0200

    feat: Setup Project

---
+++ IMEB.AppHost/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.IMEB_ApiService>("apiservice");
+
+builder.AddProject<Projects.IMEB_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
